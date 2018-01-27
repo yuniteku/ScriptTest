2d0657b9a0f6713fed48640c4fc6639ac263fcfa
yuniteku <linesabu@gmail.com>
2018-01-27 19:56:40 +0900

    C# Lesson4 課題
    Unityコース C# Lesson4 の課題と発展課題です

---
+++ Assets/ScriptTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour
+{
+    //課題1
+   private int[] array = new int[5] { 10, 20, 30, 40, 50 };
+   private Boss boss = new Boss();
+
+    // Use this for initialization
+    void Start()
+    {
+        
+
+        //課題2
+        Debug.Log(array[0]);
+        Debug.Log(array[1]);
+        Debug.Log(array[2]);
+        Debug.Log(array[3]);
+        Debug.Log(array[4]);
+
+        Test();
+
+        //発展課題
+        boss.Magic(5);
+ 
+
+    }
+
+
+    void Test()
+    {
+        //課題3
+        for (int j = array.Length - 1; 0 <= j; j--)
+        {
+            Debug.Log(array[j]);
+
+        }
+    }
+
+
+public class  Boss
+{
+        private int mp = 53;
+
+
+       public void Magic(int mAttack)
+        {
+           while(mp > 5)
+            { 
+
+                if (mp >= 4)
+                {
+
+                    this.mp -= mAttack;
+                    Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+                }
+                if (mp <= 4)
+                {
+
+                    this.mp = mAttack;
+                    Debug.Log("MPが足りないため魔法が使えない。");
+                }
+
+            }
+
+        }
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Boss lastboss = new Boss();
+
+    Boss midboss = new Boss();
+
+
+    // Use this for initialization
+    void Start()
+    {
+    
+
+        lastboss.Attack();
+
+        midboss.Attack();
+
+        lastboss.Defence(3);
+
+        midboss.Defence(1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+
+    private string[] name = { "ボス","中ボス"};
+
+    public void Attack()
+    {
+        Debug.Log(name[0] + "に"+ this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(name[1] + "から" + damage + "のダメージを受けた");
+        //残りHPを減らす
+        this.hp -= damage;
+
+    }
+
+}
+
